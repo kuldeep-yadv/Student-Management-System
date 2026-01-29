21ffab706267aad067e4ed6f5df1e8f61df8485b
kuldeep-yadv <kuldeepyadav18264@gmail.com>
2026-01-29 11:53:23 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "bhiggo", 
+  password: "pwd", 
   database: "student_db"
 });
 
